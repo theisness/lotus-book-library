68b257a970456a38fdcc9c31432e63189fa4c6b8
Huang Xin <chrox.huang@gmail.com>
2025-10-11 18:59:55 +0800

    fix: actually allow script in the transform target, closes #2200 (#2203)

--- apps/readest-app/src/app/reader/components/FoliateViewer.tsx
+++ apps/readest-app/src/app/reader/components/FoliateViewer.tsx
@@ -287,7 +287,7 @@ const FoliateViewer: React.FC<{
       book.transformTarget?.addEventListener('load', (event: Event) => {
         const { detail } = event as CustomEvent;
         if (detail.isScript) {
-          detail.allowScript = viewSettings.allowScript ?? false;
+          detail.allow = viewSettings.allowScript ?? false;
         }
       });
       const viewWidth = appService?.isMobile ? screen.width : window.innerWidth;
