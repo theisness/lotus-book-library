c3d976633ea81d0c3022b3480e3ac7405225fcb0
Huang Xin <chrox.huang@gmail.com>
2025-12-23 01:39:09 +0800

    feat(font): support OpenType feature of vrt2 and vert for vertical replacement of some punctuations (#2764)

--- apps/readest-app/src/utils/style.ts
+++ apps/readest-app/src/utils/style.ts
@@ -242,6 +242,7 @@ const getLayoutStyles = (
   }
   html, body {
     ${writingMode === 'auto' ? '' : `writing-mode: ${writingMode} !important;`}
+    ${vertical ? 'font-feature-settings: "vrt2" 1, "vert" 1; text-orientation: upright;' : ''}
     text-align: var(--default-text-align);
     max-height: unset;
   }
@@ -404,6 +405,10 @@ const getLayoutStyles = (
   div.right *, p.right * { text-align: right; }
   div.center *, p.center * { text-align: center; }
   div.justify *, p.justify * { text-align: justify; }
+
+  .br {
+    display: flow-root;
+  }
 
   .h5_mainbody {
     overflow: unset !important;
