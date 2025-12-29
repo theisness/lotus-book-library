530fa23b36b98311951c380944f06091cf5bc85b
Huang Xin <chrox.huang@gmail.com>
2025-12-29 05:12:32 +0100

    chore: bump opennext, wrangler and serwist to the latest versions (#2807)

--- apps/readest-app/src/i18n/i18n.ts
+++ apps/readest-app/src/i18n/i18n.ts
@@ -1,46 +1,57 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 import { options } from '../../i18next-scanner.config';
 
-i18n
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ['en', ...options.lngs],
-    fallbackLng: {
-      'zh-HK': ['zh-TW', 'en'],
-      kk: ['ru', 'en'],
-      ky: ['ru', 'en'],
-      tk: ['ru', 'en'],
-      uz: ['ru', 'en'],
-      ug: ['ru', 'en'],
-      tt: ['ru', 'en'],
-      default: ['en'],
-    },
-    ns: options.ns,
-    defaultNS: options.defaultNs,
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
-    detection: {
-      order: ['querystring', 'localStorage', 'navigator'],
-      caches: ['localStorage'],
-    },
-    keySeparator: false,
-    nsSeparator: false,
-    interpolation: {
-      escapeValue: false,
-    },
-    react: {
-      useSuspense: false,
-    },
-  });
+const isBrowser = typeof window !== 'undefined';
 
-i18n.on('languageChanged', (lng) => {
-  console.log('Language changed to', lng);
-});
+const initI18n = async () => {
+  if (isBrowser) {
+    const HttpApi = (await import('i18next-http-backend')).default;
+    i18n.use(HttpApi);
+  }
+
+  i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      supportedLngs: ['en', ...options.lngs],
+      fallbackLng: {
+        'zh-HK': ['zh-TW', 'en'],
+        kk: ['ru', 'en'],
+        ky: ['ru', 'en'],
+        tk: ['ru', 'en'],
+        uz: ['ru', 'en'],
+        ug: ['ru', 'en'],
+        tt: ['ru', 'en'],
+        default: ['en'],
+      },
+      ns: options.ns,
+      defaultNS: options.defaultNs,
+      ...(isBrowser && {
+        backend: {
+          loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+      }),
+      detection: {
+        order: ['querystring', 'localStorage', 'navigator'],
+        caches: ['localStorage'],
+      },
+      keySeparator: false,
+      nsSeparator: false,
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: false,
+      },
+    });
+
+  i18n.on('languageChanged', (lng) => {
+    console.log('Language changed to', lng);
+  });
+};
+
+initI18n();
 
 export default i18n;
