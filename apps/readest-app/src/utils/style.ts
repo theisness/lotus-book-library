0f23a454f76fb507025507af7b5857e0eb177b2a
Huang Xin <chrox.huang@gmail.com>
2026-01-27 15:03:17 +0100

    fix(css): no default mix blend mode for hr, closes #3086 (#3093)

--- apps/readest-app/src/utils/style.ts
+++ apps/readest-app/src/utils/style.ts
@@ -158,7 +158,7 @@ const getColorStyles = (
     *:has(> hr.background-img):not(body) {
       background-color: ${bg};
     }
-    hr {
+    hr.background-img {
       mix-blend-mode: multiply;
     }
     /* inline images */
