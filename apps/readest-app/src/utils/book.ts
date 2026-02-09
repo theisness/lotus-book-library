1f7ffa9c9cff41eab3e6bdf1cc8e74bef20387ba
Huang Xin <chrox.huang@gmail.com>
2026-02-09 10:09:52 +0800

    feat(l10n): format sync date time with current locale, closes #3219 (#3230)

--- apps/readest-app/src/utils/book.ts
+++ apps/readest-app/src/utils/book.ts
@@ -1,7 +1,7 @@
 import { BookMetadata, EXTS } from '@/libs/document';
 import { Book, BookConfig, BookProgress, WritingMode } from '@/types/book';
 import { SUPPORTED_LANGS } from '@/services/constants';
-import { getUserLang, makeSafeFilename } from './misc';
+import { getLocale, getUserLang, makeSafeFilename } from './misc';
 import { getStorageType } from './storage';
 import { getDirFromLanguage } from './rtl';
 import { code6392to6391, isValidLang, normalizedLangCode } from './lang';
@@ -179,6 +179,11 @@ export const formatDate = (date: string | number | Date | null | undefined, isUT
   } catch {
     return;
   }
+};
+
+export const formatLocaleDateTime = (date: number | Date) => {
+  const userLang = getLocale();
+  return new Date(date).toLocaleString(userLang);
 };
 
 export const formatBytes = (bytes?: number | null, locale = 'en-US') => {
